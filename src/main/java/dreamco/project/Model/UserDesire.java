ed184e52c0ed1b590246042a9b2e10ce7b0da798
ArtemVorov <temaprofit@gmail.com>
2016-09-19 14:07:05 +0300

    Add class UserDesire

---
+++ src/main/java/dreamco/project/Model/UserDesire.java
@@ -0,0 +1,40 @@
+package dreamco.project.Model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * Created by Artyom on 19.09.2016.
+ */
+public class UserDesire {
+
+    private final LocalDateTime dateTime;
+
+    private final String desireDescription;
+
+
+    public UserDesire(LocalDateTime dateTime, String desireDescription) {
+        this.dateTime = dateTime;
+        this.desireDescription = desireDescription;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDesireDescription() {
+        return desireDescription;
+    }
+
+
+    public LocalDate getDate() {
+        return dateTime.toLocalDate();
+    }
+
+    public LocalTime getTime() {
+        return dateTime.toLocalTime();
+    }
+
+
+}
