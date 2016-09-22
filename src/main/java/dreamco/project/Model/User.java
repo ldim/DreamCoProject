6af4e2d3bdd91fb6301f6feba7b966b56cb7e6d6
ArtemVorov <temaprofit@gmail.com>
2016-09-22 16:48:59 +0300

    .ideaRm

---
+++ src/main/java/dreamco/project/Model/User.java
@@ -0,0 +1,42 @@
+package dreamco.project.Model;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Artyom on 20.09.2016.
+ */
+public class User {
+    private AtomicInteger userId;
+    private int userPassword;
+    private String userEmail;
+
+    public User(AtomicInteger userId, int userPassword, String userEmail) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.userEmail = userEmail;
+    }
+
+    public AtomicInteger getUserId() {
+        return userId;
+    }
+
+    public int getUserPassword() {
+        return userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserId(AtomicInteger userId) {
+        this.userId = userId;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public void setUserPassword(int userPassword) {
+        this.userPassword = userPassword;
+    }
+}
