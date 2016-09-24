934e4ddd4ba91de66521a2fc6d6fd34530ddef08
ArtemVorov <temaprofit@gmail.com>
2016-09-24 13:05:28 +0300

    Add servlet & logger

--- src/main/java/dreamco/project/web/UserServlet.java
+++ src/main/java/dreamco/project/web/UserServlet.java
@@ -1,17 +1,23 @@
 package dreamco.project.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * Created by Artyom on 23.09.2016.
  */
 public class UserServlet extends HttpServlet {
-
+    private static final Logger LOG = getLogger(UserServlet.class);
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+        LOG.debug("redirect to userList");
+        //request.getRequestDispatcher("/userList.jsp").forward(request, response);
+        response.sendRedirect("userList.jsp");
     }
 }
